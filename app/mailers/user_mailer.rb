213303a5d5b52f925e20b6b26ebfc799cf2bd1fb
Alena Gramenko <alena.gramenko@gmail.com>
2018-06-19 13:54:47 +0300

    fixes1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @url = "http://localhost:3000/users/sign_in"
+    @url = "#{root_url}users/sign_in"
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
   
